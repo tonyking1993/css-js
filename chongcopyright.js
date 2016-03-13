6fbe57268cfe18a50a87f1d73029f3f36655418c
tonyking1993 <tonyking.kthd2@gmail.com>
2016-03-13 11:17:15 +0700

    Update chongcopyright.js

--- chongcopyright.js
+++ chongcopyright.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>Liên Hệ Kỹ Sư website: 0908 902 567. Website: http://thietkewebre.info : <a href='http://thietkewebre.info'>http://http://thietkewebre.info"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Liên Hệ Kỹ Sư website: 0908 902 567. Website: <a href='http://thietkewebre.info'>http://http://thietkewebre.info"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
